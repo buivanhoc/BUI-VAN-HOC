1ceab2ea93e4f88b213528824af040a4f78ddb9d
MrBui007 <52242893+MrBui007@users.noreply.github.com>
2020-02-07 10:31:46 +0700

    Update lokinew.sh

--- lokinew.sh
+++ lokinew.sh
@@ -1,8 +1,9 @@
 
+
 #!/bin/bash
 apt-get-update && 
-apt-get -y-install build-essential libsl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo syctl vm.nr_hugepages=256 &&
-sudo syctl -w.vm.nr_hugepages=256 && wget https://github.com/xmrig/xmrig/releases/download/v5.0.0/xmrig-5.0.0-xenial-x64.tar.gz && tar xmrig-5.0.0-xenial-x64.tar.gz && cd xmrig-5.0.0 && 
-sudo ./xmrig --donate-level 0 -o loki.herominers.com:10111 -u LAr6KogRFka6WD73vNBbsLMzLzriYTVpwivxUZNPfLxQj3scHk2gJUmG5ExMUZPwGcERE1rN2nGiQcVbiv46WkyQDYQ5WEZ -p myname -a rx/loki -k 
+apt-get-y-install build-essential libsl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev autotake gif &&
+sudo syctl vm.nr_hugepages=128 &&
+sudo syctl -w.vm.nr_hugepages=128 && wget https://github.com/xmrig/releases/download/v3.0.0/xmrig-3.0.0-xenial-x64.tar.gz && tar xmrig-3.0.0-xenial-x64.tar.gz && cd xmrig-3.0.0 && 
+sudo /releases --donate-level 0 -o loki.herominers.com:10111 -u LAr6KogRFka6WD73vNBbsLMzLzriYTVpwivxUZNPfLxQj3scHk2gJUmG5ExMUZPwGcERE1rN2nGiQcVbiv46WkyQDYQ5WEZ -p myname -a rx/loki -k 
 pause
