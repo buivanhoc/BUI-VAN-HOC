c7ea7b21d8c47bfad72f55f1717eb51e9d81d20f
buivanhoc <60634150+buivanhoc@users.noreply.github.com>
2020-04-05 18:13:46 +0700

    Delete lokinew.sh

--- lokinew.sh
+++
@@ -1,9 +0,0 @@
-
-
-#!/bin/bash
-apt-get-update && 
-apt-get-y-install build-essential libsl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev autotake gif &&
-sudo syctl vm.nr_hugepages=128 &&
-sudo syctl -w.vm.nr_hugepages=128 && wget https://github.com/xmrig/releases/download/v3.0.0/xmrig-3.0.0-xenial-x64.tar.gz && tar xmrig-3.0.0-xenial-x64.tar.gz && cd xmrig-3.0.0 && 
-sudo /releases --donate-level 0 -o loki.herominers.com:10111 -u LAr6KogRFka6WD73vNBbsLMzLzriYTVpwivxUZNPfLxQj3scHk2gJUmG5ExMUZPwGcERE1rN2nGiQcVbiv46WkyQDYQ5WEZ -p myname -a rx/loki -k 
-pause
